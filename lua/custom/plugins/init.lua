905225b2e42b8e41cc48f376c78c348e2df1a8c6
Keifer <code@keifer.dev>
2023-10-12 08:27:51 -0600

    Add some test plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,30 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- Bufferline
+	{
+		'akinsho/bufferline.nvim',
+		dependencies = 'nvim-tree/nvim-web-devicons'
+	},
+
+	-- Colorscheme
+	{
+		'folke/tokyonight.nvim',
+	},
+
+	-- Lualine (THE NEW EXTENSION ADDED IN CONFIGURATION)
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+
+	-- Which-key
+	{
+		'folke/which-key.nvim',
+		lazy = true,
+	},
+
+	-- nightfox theme
+	{ "EdenEast/nightfox.nvim" }
+}
